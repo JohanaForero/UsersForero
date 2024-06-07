fad459fd89f00e111ea8bd7887a4aba903b0d8c2
Johana Forero Mendoza <johanaforeromendoza31@gmail.com>
2024-06-07 21:08:28 +0200

    Implementation of the Users service.

--- src/main/java/com/forero/infrastructure/configuration/UserBeanConfiguration.java
+++ src/main/java/com/forero/infrastructure/configuration/UserBeanConfiguration.java
@@ -5,13 +5,9 @@ import com.forero.application.query.UserQuery;
 import com.forero.application.query.UsersQuery;
 import com.forero.application.service.UserService;
 import com.forero.application.usecase.UserUseCase;
-import com.mongodb.reactivestreams.client.MongoClients;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.data.mongodb.ReactiveMongoDatabaseFactory;
-import org.springframework.data.mongodb.core.ReactiveMongoTemplate;
-import org.springframework.data.mongodb.core.SimpleReactiveMongoDatabaseFactory;
 
 @Configuration
 @RequiredArgsConstructor
@@ -36,16 +32,5 @@ public class UserBeanConfiguration {
     @Bean
     public UserQuery userQuery(final UserUseCase userUseCase) {
         return new UserQuery(userUseCase);
-    }
-
-    @Bean
-    public ReactiveMongoDatabaseFactory reactiveMongoDatabaseFactory() {
-        return new SimpleReactiveMongoDatabaseFactory(MongoClients.create(this.mongoDBProperties.getUri()),
-                this.mongoDBProperties.getDatabase());
-    }
-
-    @Bean
-    public ReactiveMongoTemplate reactiveMongoTemplate() {
-        return new ReactiveMongoTemplate(this.reactiveMongoDatabaseFactory());
     }
 }
