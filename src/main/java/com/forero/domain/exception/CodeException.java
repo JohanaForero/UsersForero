255018444d36dcf3dae06b94ad1866cbcd47f3d7
Johana Forero Mendoza <johanaforeromendoza31@gmail.com>
2024-06-18 19:35:10 +0200

    Implementation of the Users service.

--- src/main/java/com/forero/domain/exception/CodeException.java
+++ src/main/java/com/forero/domain/exception/CodeException.java
@@ -8,7 +8,7 @@ import lombok.Getter;
 public enum CodeException {
     INTERNAL_SERVER_ERROR("Internal Server Error"),
     USER_NOT_FOUND("User not found"),
-    DB_INTERNAL("Unavailable service"),
+    DB_INTERNAL("Unavailable DB service"),
     INVALID_PARAMETERS("Invalid %s Parameters");
     private final String messageFormat;
 }
